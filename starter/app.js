86e5827d370ebd956ca3e076985e3d16af8d3991
unknown <sakib3@gmail.com>
2015-05-31 23:51:53 +0200

    Add styles for the new elements and javascript so that we can get the info of the selected item

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,9 @@
+// The <paper-tabs> element fires a core-select event when you select a tab. 
+//You can interact with the element just like a built-in element.
+//Right now there's nothing to switch; you'll finish hooking it up later.
+var tabs = document.querySelector('paper-tabs');
 
+tabs.addEventListener('core-select', function(event) {
+  console.log("Selected: " + tabs.selected);
+  console.log(event.detail);
+});
