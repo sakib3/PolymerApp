ef0c65019cf0764764fd6f8eab8b8cccba118db1
sakib3 <sakib3@gmail.com>
2015-06-06 18:31:47 +0200

    Finishing touches so that tapped favourites will be available in favourites panel

--- www/app.js
+++ www/app.js
@@ -2,8 +2,9 @@
 //You can interact with the element just like a built-in element.
 //Right now there's nothing to switch; you'll finish hooking it up later.
 var tabs = document.querySelector('paper-tabs');
-
+var list = document.querySelector('post-list');
 tabs.addEventListener('core-select', function(event) {
   console.log("Selected: " + tabs.selected);
   console.log(event.detail);
+  list.show = tabs.selected
 });
